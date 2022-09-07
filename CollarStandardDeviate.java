340d3a6a0855aca88b566855a6b9526d040ffd60
06megha12 <112416048+06megha12@users.noreply.github.com>
2022-09-07 10:02:36 +0530

    Add files via upload

---
+++ CollarStandardDeviate.java
@@ -0,0 +1,14 @@
+package com.CollarStandardDeviate;
+
+public class CollarStandardDeviate extends Exception{
+
+	  public CollarStandardDeviate(String message) {
+
+	        super(message);
+
+	        // TODO Auto-generated constructor stub
+
+	    }
+
+
+}
---
+++ ItemPurchase.java
@@ -0,0 +1,9 @@
+package com.item.exmple;
+
+public class ItemPurchase extends Exception {
+
+	public ItemPurchase(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
